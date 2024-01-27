80c623522591b49159689862b0c8e208888e5147
nandansrikrishna <nandans@umich.edu>
2024-01-27 17:26:45 -0500

    Proper Routing Format with Root Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import LandingPage from "./LandingPage.jsx";
-import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div style={{ overflow: "hidden" }}>
-              <LandingPage />
-            </div>,
-  },
-]);
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes.jsx";
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <div style={{ overflow: "hidden" }}>
-      <LandingPage />
-    </div>
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>
 );
