90f63d0f19012450907c9a1362f24f47f7967115
nandansrikrishna <nandans@umich.edu>
2024-01-27 17:50:39 -0500

    Basic Login Working

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../components/LandingPage";
+import Login from "../components/Login"
+import Dashboard from "../components/dashboard";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
