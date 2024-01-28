3a35b0b4864bc7e084b450ea40323a4e015277b4
AlexWangFTC <alexw2002mi@gmail.com>
2024-01-27 20:01:46 -0500

    added the modules routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "../components/LandingPage";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
+import Modules from "../components/Modules"
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/modules" element={<Modules />} />
     </Routes>
   );
 };
